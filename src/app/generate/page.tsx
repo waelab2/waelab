2ab84d7059006c02159322a18070ee031fdf0671
Mosaab El-Emam <mosaab.emam123@outlook.com>
2025-06-26 11:19:12 -0400

    feat: minimal test of fal api

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,7 +1,41 @@
+"use client";
+
+import { fal } from "@fal-ai/client";
+import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Textarea } from "~/components/ui/textarea";
 
 export default function GeneratePage() {
+  const [prompt, setPrompt] = useState<string>("");
+
+  fal.config({
+    proxyUrl: "/api/fal/proxy",
+  });
+
+  async function handleSubmit(prompt: string) {
+    console.log("handleSubmit", prompt);
+
+    if (!prompt) {
+      throw new Error("Prompt is required");
+    }
+
+    const result = await fal.subscribe("fal-ai/flux/dev", {
+      input: {
+        prompt,
+        image_size: "square_hd",
+      },
+      pollInterval: 5000,
+      logs: true,
+      onQueueUpdate(update) {
+        console.log("queue update", update);
+      },
+    });
+
+    console.log("result", result);
+    console.log("###########");
+    // console.log("result.images", result.images);
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
       <div className="mx-auto w-full max-w-2xl">
@@ -13,8 +47,12 @@ export default function GeneratePage() {
             <Textarea
               placeholder="Type your message here..."
               className="min-h-[150px] resize-none"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
             />
-            <Button className="w-full">Generate</Button>
+            <Button className="w-full" onClick={() => handleSubmit(prompt)}>
+              Generate
+            </Button>
           </div>
         </div>
       </div>
